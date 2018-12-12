a7c910f857d3277a7bdec5bfb71febcca71e8144
Sorin Dragomir <sorinel.dragomir@ownzones.com>
2018-12-12 15:58:47 +0200

    48 - 11 si 54

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,8 +3,10 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 import { getCurrentProfile } from '../../actions/profileActions'
+import Spinner from '../common/Spinner';
 
 class Dashboard extends Component {
+
 
   // we want getCurrentProfile to be called right away
   componentDidMount() {
@@ -12,9 +14,32 @@ class Dashboard extends Component {
   }
 
   render() {
+    // Check if profile state is not null, before we render anything
+
+    let dashboardContent;
+    const user = this.props.auth;
+    const { profile, loading } = this.props.profile;
+
+
+    if (profile === null || loading === true) {
+      // profile will be null in the begining because getCurrentProfile() is called rigth away
+      // and has default value null in reducer
+      dashboardContent = <Spinner />;
+
+    } else {
+      // Checked if logged user has profile data
+    }
     return (
-      <div>
-        <h1>Dashboard</h1>
+      <div className="dashboard">
+        <div className="contaner">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4">Dashboard
+                {dashboardContent}
+              </h1>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
@@ -22,13 +47,18 @@ class Dashboard extends Component {
 const mapStateToProps = (state) => {
   return {
     profile: state.profile,
-    errors: state.errors
+    auth: state.auth
   }
 }
 
 Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
-  errors: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+}
+
+Dashboard.defaultProps = {
+
 }
 
 export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
